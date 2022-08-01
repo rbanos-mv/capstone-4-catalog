90b0941710f356a8915c66ec01cb619f8177a250
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-01 12:26:58 -0500

    Create schema skeleton

---
+++ schema/schema.sql
@@ -0,0 +1,14 @@
+-- CREATE DATABASE
+CREATE DATABASE catalog_of_things;
+
+-- 3 CREATE TABLE author
+
+-- 1 CREATE TABLE label
+
+-- 2 CREATE TABLE genre
+
+-- 1 CREATE TABLE book
+
+-- 3 CREATE TABLE game
+
+-- 2 CREATE TABLE music_album
