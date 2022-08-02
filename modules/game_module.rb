468d0e8c773b7da3a6d2053853f8c8d33eb51784
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-02 17:00:42 -0300

    Add 'add_game' method

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -7,6 +7,23 @@ module GameModule
 
   def add_game
     # implementation
+    title = string_input('Title of the game: ')
+    publish_date = date_input('Publication date: ')
+    last_played_at = date_input('Last played date: ')
+    multiplayer = boolean_input('Is multiplayer? (y/n): ')
+    game = Game.new(title, publish_date, last_played_at, multiplayer)
+    author_first_name = string_input('Author First Name: ')
+    author_last_name = string_input('Author Last Name: ')
+    author = Author.new(author_first_name, author_last_name)
+    game.author = author
+    genre_name = string_input('Genre: ')
+    genre = Genre.new(genre_name)
+    game.genre = genre
+    label_title = string_input('Label Title: ')
+    label_color = string_input('Label color: ')
+    label = Label.new(label_title, label_color)
+    game.label = label
+    game
   end
 
   def list_games
