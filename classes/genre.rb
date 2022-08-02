d82dbe541c1f03586992b05f28dd89ea0add56a3
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-02 11:31:59 -0500

    Add Genre class

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,11 +1,18 @@
 class Genre
   # Team member 2
+  attr_reader :id, :name
 
-  def initialize
-    # constructor
+  def initialize(name, id = Random.rand(1..1000))
+    @name = name
+    @id = id
+    @items = []
   end
 
   def add_item(item)
-    # implementation
+    @items << item unless @items.any? { |a| a.id == item.id }
+  end
+
+  def to_s
+    "id: #{id}  name: #{name}"
   end
 end
