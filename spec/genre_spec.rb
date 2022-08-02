87212aff7fb9f55186471cb4ccdea302e056a092
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-02 11:31:11 -0500

    Add tests for Genre class

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Genre do
   it 'should throw an ArgumentError if incorrect number of parameters' do
     expect { Genre.new }.to raise_error(ArgumentError)
   end
+
+  it "should have id: #{genre_id}" do
+    genre = create_genre
+
+    result = genre.id
+
+    expect(result).to eq(genre_id)
+  end
+
+  it "should have name: #{genre_name}" do
+    genre = create_genre
+
+    result = genre.name
+
+    expect(result).to eq(genre_name)
+  end
 end
--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,5 +1,6 @@
 require 'json'
 require_relative '../classes/book'
+require_relative '../classes/genre'
 
 # *****************
 # Team member 1
@@ -12,6 +13,24 @@ require_relative '../classes/book'
 # Team member 2
 # *****************
 # genre
+
+def genre_id
+  1
+end
+
+def genre_name
+  'Genre Name'
+end
+
+def create_genre
+  Genre.new(genre_name, genre_id)
+end
+
+def genre_mock
+  mock = double('Genre', { name: genre_name, id: genre_id })
+  allow(mock).to receive(:add_item)
+  mock
+end
 
 # music_album
 
