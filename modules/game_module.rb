315788360a9b0e35f9bc72b0ac4304d70dea3657
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-02 18:49:44 -0300

    Use ask_game_data

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -7,25 +7,29 @@ module GameModule
   # Team member 3
 
   def ask_game_data
-    # implementation
-  end
-
-  def add_game
-    # implementation
     title = string_input('Title of the game: ')
     publish_date = date_input('Publication date (yyyy-mm-dd): ')
     last_played_at = date_input('Last played date (yyyy-mm-dd): ')
     multiplayer = boolean_input('Is multiplayer? (y/n): ')
-    game = Game.new(title, publish_date, last_played_at, multiplayer)
+
     author_first_name = string_input('Author First Name: ')
     author_last_name = string_input('Author Last Name: ')
-    author = Author.new(author_first_name, author_last_name)
-    game.author = author
     genre_name = string_input('Genre: ')
-    genre = Genre.new(genre_name)
-    game.genre = genre
     label_title = string_input('Label Title: ')
     label_color = string_input('Label color: ')
+    [title, publish_date, last_played_at, multiplayer, author_first_name, author_last_name, \
+     genre_name, label_title, label_color]
+  end
+
+  def add_game
+    title, publish_date, last_played_at, multiplayer, author_first_name, author_last_name, \
+genre_name, label_title, label_color = ask_game_data
+
+    game = Game.new(title, publish_date, last_played_at, multiplayer)
+    author = Author.new(author_first_name, author_last_name)
+    game.author = author
+    genre = Genre.new(genre_name)
+    game.genre = genre
     label = Label.new(label_title, label_color)
     game.label = label
     @games.push(game)
