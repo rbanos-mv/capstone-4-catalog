5979c9ac5da3bfc15405226d47141eecd157c204
Juan Rosario <jrosario19@hotmail.com>
2022-08-02 10:39:27 -0400

    Add label class

--- classes/label.rb
+++ classes/label.rb
@@ -1,11 +1,19 @@
 class Label
   # Team member 1
+  attr_reader :id, :items
+  attr_accessor :title, :color
 
-  def initialize
+  def initialize(id, title, color)
     # constructor
+    @id = id
+    @title = title
+    @color = color
+    @items = []
   end
 
   def add_item(item)
     # implementation
+    @item = item
+    item.label << self
   end
 end
