f4010e075d32b1de3e330398bd78706c53be5710
Juan Rosario <jrosario19@hotmail.com>
2022-08-02 10:24:57 -0400

    Add book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,13 +1,17 @@
-class Book
-  # Team member 1
+require_relative 'item'
 
-  def initialize
-    # constructor
+class Book < Item
+  attr_reader :publisher, :cover_state
+  attr_accessor :published_date
+
+  def initialize(published_date, publisher, cover_state)
+    super(id, published_date)
+    @id = rand(1..1000)
+    @publisher = publisher
+    @cover_state = cover_state
   end
 
-  private
-
   def can_be_archived?
-    # implementation
+    super || @cover_state == 'bad'
   end
 end
