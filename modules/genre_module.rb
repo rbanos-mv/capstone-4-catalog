5c6c91d9998ad621eaf9d0242118ec5f24a3a1a6
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-03 07:03:51 -0500

    Revert "Implement GenreModule"
    This reverts commit 562fbe1c0f37690901916ab8efc205190fdf6007.

--- modules/genre_module.rb
+++ modules/genre_module.rb
@@ -2,40 +2,14 @@ module GenreModule
   # Team member 2
 
   def ask_genre_data
-    string_input('Genre name: ')
+    # implementation
   end
 
   def add_genre
-    genre_name = ask_genre_data
-    @genres << Genre.new(genre_name)
-  end
-
-  def select_genre
-    loop do
-      prompt = "    0) Add Genre\n#{genres_list}\nSelect a Genre from the list: "
-      option = numeric_input(prompt, (-1..@genres.length))
-      return option - 1 unless option.zero?
-
-      add_genre
-    end
-  end
-
-  def genres_list
-    @genres.map.with_index do |genre, index|
-      index_str = (index + 1).to_s.rjust(5)
-      "#{index_str}) #{genre.to_s(full: false)}\n"
-    end.join
+    # implementation
   end
 
   def list_genres
-    header, line = Genre.header(empty: @genres.empty?)
-    if @genres.empty?
-      puts header
-    else
-      list = @genres.map do |genre|
-        genre
-      end.join("\n")
-      puts "#{header}\n#{list}\n#{line}"
-    end
+    # implementation
   end
 end
