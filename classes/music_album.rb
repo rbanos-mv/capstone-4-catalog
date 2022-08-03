06fc9bd25ad3cbf3f38bde02caba8e50811c7d77
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-03 07:22:53 -0500

    Add methods to MusicAlbum class - self.name - self.header -to_s

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -9,9 +9,35 @@ class MusicAlbum < Item
     @on_spotify = on_spotify
   end
 
-  def to_s
-    "id: #{id.to_s.rjust(4)} genre: #{genre.name} title: #{genre.title} publish date: #{publish_date} \
-on spotify: #{on_spotify ? 'YES' : 'NO'}"
+  def self.name
+    'MUSIC ALBUMS'
+  end
+
+  def self.header(full: true, margin: 0, empty: false)
+    id_col = full ? '|    ID ' : ''
+    genre_col = 'GENRE'.ljust(30)
+    title_col = 'TITLE'.ljust(30)
+    publish_col = 'PUBLISHED'.center(10)
+    spotify_col = 'SPOTIFY'.center(7)
+
+    row = "#{id_col}| #{genre_col} | #{title_col} | #{publish_col} | #{spotify_col} |"
+    width = row.length + margin
+    line = ''.center(row.length, '-').rjust(width)
+    row = '*** EMPTY LIST ***'.center(row.length) if empty
+    entity_name = MusicAlbum.name.center(row.length)
+
+    ["\n#{entity_name}\n#{line}\n#{row.rjust(width)}\n#{line}", line]
+  end
+
+  def to_s(full: true, margin: 0)
+    id_col = full ? "| #{id.to_s.rjust(5)} " : ''
+    genre_col = genre.name.ljust(30)
+    title_col = title.ljust(30)
+    publish_col = publish_date.to_s.center(10)
+    spotify_col = (on_spotify ? 'YES' : 'NO').center(7)
+
+    row = "#{id_col}| #{genre_col} | #{title_col} | #{publish_col} | #{spotify_col} |"
+    row.rjust(row.length + margin)
   end
 
   private
