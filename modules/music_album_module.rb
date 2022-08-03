58279161fb1919ad87088df0ddf5f8783851cd6d
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-03 07:02:54 -0500

    Revert "Implement MusicAlbumModule"
    This reverts commit a46fdfb23934fbb23ddbc8b18e8d6c08c3e80416.

--- modules/music_album_module.rb
+++ modules/music_album_module.rb
@@ -1,36 +1,15 @@
-require_relative '../classes/genre'
-require_relative '../classes/music_album'
-
 module MusicAlbumModule
   # Team member 2
 
   def ask_music_album_data
-    head = 'ADD MUSIC ALBUM'.center(50)
-    puts "\n#{head}"
-    [
-      string_input('Album title: '),
-      date_input('Publish date: '),
-      letter_input('Is it on Spotify? (Y/N) ', %w[Y N]),
-      select_genre
-    ]
+    # implementation
   end
 
   def add_music_album
-    title, publish_date, on_spotify, genre = ask_music_album_data
-    album = MusicAlbum.new(title, publish_date, on_spotify: on_spotify)
-    album.genre = @genres[genre]
-    @albums << album
+    # implementation
   end
 
   def list_music_albums
-    header, line = MusicAlbum.header(empty: @albums.empty?)
-    if @albums.empty?
-      puts header
-    else
-      list = @albums.map do |album|
-        album
-      end.join("\n")
-      puts "#{header}\n#{list}\n#{line}"
-    end
+    # implementation
   end
 end
